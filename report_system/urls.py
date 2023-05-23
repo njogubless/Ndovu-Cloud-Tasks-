0c136423a86abc322433402347751ecaefc5ac66
GeoVolunteer <pnjogubless@gmail.com>
2023-05-23 20:40:50 +0300

    creating the models and listing the necesary urls

---
+++ report_system/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for report_system project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('department/<int:department_id>/', views.department_report, name='department_report'),
+    path('report/<int:report_id>/', views.report_details, name='report_details'),
+    path('report/create/', views.create_report, name='create_report'),
+]
