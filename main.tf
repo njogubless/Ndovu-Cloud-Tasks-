93da011661ec359d61db08d8e273178b0155c367
GeoVolunteer <pnjogubless@gmail.com>
2023-06-01 20:16:53 +0300

    edited the main.tf file and initialised terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 # Configure the DigitalOcean provider
 provider "digitalocean" {
-  token = "YOUR_DIGITALOCEAN_API_TOKEN"
+  token = "dop_v1_0b61b0ea9f0837e921396a2d67c3f7cf23be3a17e5e4daf39e275ff4786ee24a"
 }
 
 # Create a new droplet
 resource "digitalocean_droplet" "example" {
-  image    = "ubuntu-20-04-x64"
-  name     = "example-droplet"
-  region   = "nyc1"
-  size     = "s-1vcpu-1gb"
+  image  = "Ubuntu 22.04 (LTS) x64"
+  name   = "Bless"
+  region = "TOR1"
+  size   = "s-1vcpu-1gb"
+}
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
 }
